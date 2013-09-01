ad343857e6542d1ba62bd0c5e63c0527e27c2941
Rakesh Behera <rakesh8248@gmail.com>
2013-09-01 16:41:58 +0530

    Added details to createTrajectoryKML.R

--- createTrajectoryKML.R
+++ createTrajectoryKML.R
@@ -1,3 +1,11 @@
+# Script:   createTrajectoryKML.R
+# Author:   Rakesh Behera
+# Date:   16/08/13
+# Modified: 19/08/13
+# Purpose:  This script reads a typical GPS log CSV file into a data frame,
+#       creates an sp object out of it, assigns a CRS, creates an STIDF
+#       and plots the KML by triggering Google Earth
+
 createTrajectoryKML <- function(tripFile = "SampleGPSData.csv"){
   library(sp);
   library(plotKML);
